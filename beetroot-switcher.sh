095fc6eef8791bbaf88102fd46554c7f94fca245
Ian Jordan <immoloism@gmail.com>
2024-11-22 10:52:35 +0000

    beetroot-switcher.sh: Fix issues
    Removed whitespace and added POSIX compliant shebang.

--- beetroot-switcher.sh
+++ beetroot-switcher.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
+
 while true; do
-        gsettings set org.mate.background picture-filename "/home/immolo/Pictures/1.jpg"
-        sleep 60
-        gsettings set org.mate.background picture-filename "/home/immolo/Pictures/2.jpg"
-        sleep 60
-        gsettings set org.mate.background picture-filename "/home/immolo/Pictures/3.jpg"
-        sleep 60
+	gsettings set org.mate.background picture-filename "/home/immolo/Pictures/1.jpg"
+	sleep 60
+	gsettings set org.mate.background picture-filename "/home/immolo/Pictures/2.jpg"
+	sleep 60
+	gsettings set org.mate.background picture-filename "/home/immolo/Pictures/3.jpg"
+	sleep 60
 done
